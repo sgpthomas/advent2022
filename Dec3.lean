549a33ac90cd325f64435f897056037412d6dd5f
Samuel Thomas <sgpthomas@gmail.com>
2022-12-10 13:27:28 -0600

    started dec 3

--- Dec3.lean
+++ Dec3.lean
@@ -1,7 +1,36 @@
 namespace Dec3
 
+universe u
+
+def Common {α : Type u} (e : α) (a : List α) (b : List α) := e ∈ a ∧ e ∈ b
+
+theorem ex : Common 1 [2, 1] [1, 3] :=
+  by
+    simp [Common]
+    
+-- inductive Unique (α : Type u) where
+    
+inductive Rucksack (α : Type u) where
+| nil : (e : α) -> Rucksack α
+| cons : (p : α × α) -> Rucksack α -> Rucksack α
+deriving Repr
+
+instance (α : Type u) : HAppend (α × α) (Rucksack α) (Rucksack α) where
+  hAppend := Rucksack.cons
+  
+def fromList {α : Type u} (rs : List (α × α)) (e : α) : Rucksack α :=
+  List.foldr (λ e a => Rucksack.cons e a) (Rucksack.nil e) rs
+  
+#eval fromList [(1, 2), (1, 2), (5, 4)] 4
+
+-- I want the members of Rucksack 4 to be a pair of lists that both contain the element 4
+
+
 def parse : IO (List String) := do
   let text <- IO.FS.readFile "data/dec3.txt"
+  pure []
+  
+def validate (input : List String) := false
 
 def run : IO Unit := do
   let part1 := "todo"
